deebc2b52373d4fae0f77b83bc9bb5c24408e6f5
xdev1 <xdev1.github@gmail.com>
2022-10-09 01:34:45 +0200

    Added storybook features

---
+++ src/stories/shoelace/shoelace-date-picker.stories.ts
@@ -0,0 +1,9 @@
+import { DatePicker } from '../../main/calendar-widgets-shoelace';
+
+export default {
+  title: 'Shoelace'
+};
+
+void DatePicker;
+
+export const datePicker = () => '<sx-date-picker></sx-date-picker>';
