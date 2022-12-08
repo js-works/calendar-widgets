1d633e7c3ec6621766710f151f0f0d34b5171e3a
xdev1 <xdev1.github@gmail.com>
2022-12-08 09:36:01 +0100

    forms

--- src/demos/form.demo.ts
+++ src/demos/form.demo.ts
@@ -17,15 +17,65 @@ class FormDemo extends LitElement {
     void [SlButton, SlCard, SlInput];
   }
 
+  private _onFormSubmit = (ev: Event) => {
+    ev.preventDefault();
+    alert('All field valid');
+  };
+
   render() {
     return html`
+      <style>
+        .validity-styles sl-input,
+        .validity-styles sl-select {
+          margin-bottom: var(--sl-spacing-medium);
+        }
+
+        /* user invalid styles */
+        .validity-styles sl-input[data-user-invalid]::part(base),
+        .validity-styles sl-select[data-user-invalid]::part(control) {
+          border-color: var(--sl-color-danger-600);
+        }
+
+        .validity-styles [data-user-invalid]::part(form-control-label),
+        .validity-styles [data-user-invalid]::part(form-control-help-text) {
+          color: var(--sl-color-danger-700);
+        }
+
+        .validity-styles sl-input:focus-within[data-user-invalid]::part(base),
+        .validity-styles
+          sl-select:focus-within[data-user-invalid]::part(control) {
+          border-color: var(--sl-color-danger-600);
+          box-shadow: 0 0 0 var(--sl-focus-ring-width)
+            var(--sl-color-danger-300);
+        }
+
+        /* User valid styles */
+        .validity-styles sl-input[data-user-valid]::part(base),
+        .validity-styles sl-select[data-user-valid]::part(control) {
+          border-color: var(--sl-color-success-600);
+        }
+
+        .validity-styles [data-user-valid]::part(form-control-label),
+        .validity-styles [data-user-valid]::part(form-control-help-text) {
+          color: var(--sl-color-success-700);
+        }
+
+        .validity-styles sl-input:focus-within[data-user-valid]::part(base),
+        .validity-styles
+          sl-select:focus-within[data-user-valid]::part(control) {
+          border-color: var(--sl-color-success-600);
+          box-shadow: 0 0 0 var(--sl-focus-ring-width)
+            var(--sl-color-success-300);
+        }
+      </style>
       <sl-card>
         <div slot="header">Form demo</div>
-        <form>
+        <form class="validity-styles" @submit=${this._onFormSubmit}>
           <sl-input name="firstName" label="First name" required></sl-input>
           <sl-input name="lastName" label="Last name" required></sl-input>
           <sl-input name="city" label="City" required></sl-input>
           <br />
+          <sl-button type="reset">Reset</sl-button>
           <sl-button type="submit" variant="primary">Submit</sl-button>
         </form>
       </sl-card>
